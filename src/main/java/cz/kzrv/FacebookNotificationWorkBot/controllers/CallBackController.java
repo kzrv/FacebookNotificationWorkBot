ee5c3d0d23cde3d81736af5a0b153332f9f0107c
kzrv <kzrv.vasya@gmail.com>
2022-09-06 15:27:10 +0200

    Update

---
+++ src/main/java/cz/kzrv/FacebookNotificationWorkBot/controllers/CallBackController.java
@@ -0,0 +1,19 @@
+package cz.kzrv.FacebookNotificationWorkBot.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping()
+public class CallBackController {
+
+    @GetMapping("private_policy")
+    public String privatePolicy(){
+        return "/info/privatePolicy";
+    }
+    @GetMapping()
+    public String info(){
+        return "/info/info";
+    }
+}
